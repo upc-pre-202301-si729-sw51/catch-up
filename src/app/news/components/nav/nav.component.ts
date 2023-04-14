45abd488eb71401d0b967f42319620a1f2101c54
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-14 08:45:51 -0500

    feat: implemented nav behavior and styles.

--- src/app/news/components/nav/nav.component.ts
+++ src/app/news/components/nav/nav.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NewsApiService} from "../../services/news-api.service";
+import {LogoApiService} from "../../../core/services/logo-api.service";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  sources: Array<any> = [];
+  articles: Array<any> = [];
+  constructor(private newsApi: NewsApiService,
+              private logoApi: LogoApiService) {
+  }
+
+  searchArticlesForSource(source: any) {
+    console.log(`selected source is ${source.id}`);
+    this.newsApi.getArticlesBySourceId(source.id)
+      .subscribe((data: any) => {
+        this.articles = data['articles'];
+        this.articles.map(article => {
+          article.source.urlToLogo = source.urlToLogo;
+          article.source.url = source.url;
+        });
+        console.log(this.articles);
+      });
+  }
+
+  onSourceSelected(source: any) {
+    console.log(source.name);
+    this.searchArticlesForSource(source);
+  }
+
+  ngOnInit() {
+    this.newsApi.getSources().subscribe( (data: any) => {
+      this.sources = data['sources'];
+      this.sources.map(source => source.urlToLogo =
+        this.logoApi.getUrlToLogo(source));
+      console.log(this.sources);
+      this.searchArticlesForSource(this.sources[0]);
+    });
+  }
 
 }
