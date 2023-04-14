d8e96046633f6edbc76d57fa437a99da1864277e
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-14 08:04:30 -0500

    feat: news api behavior implemented.

--- src/app/news/services/news-api.service.ts
+++ src/app/news/services/news-api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
+  apiKey = "fecf4feeffa64e4da682e7d268612ce5"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getArticlesBySourceId(sourceId: string) {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=${sourceId}&apiKey=${this.apiKey}`);
+  }
+
+  getSources() {
+    return this.http.get(`https://newsapi.org/v2/top-headlines/sources?apiKey=${this.apiKey}`);
+  }
+
+  initArticles() {
+    return this.getArticlesBySourceId('bbc-news');
+  }
 }
