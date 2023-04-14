a93753db77d5c4d199779fee76ffa4100acb09f0
Angel Velasquez <pcisavel@upc.edu.pe>
2023-04-14 08:30:32 -0500

    feat: implemented footer and main content sections.

--- src/app/news/components/main-content/main-content.component.ts
+++ src/app/news/components/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent {
+  title = 'catch-up';
+  @Input() articles: Array<any> = [];
 
 }
